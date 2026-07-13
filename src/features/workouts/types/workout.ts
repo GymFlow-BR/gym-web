15b3de770deb2ee9f19f028e5ac8306750c67256
SKDEV9 <skgomes06@gmail.com>
2026-07-13 16:15:17 -0300

    feat: update workout

--- src/features/workouts/types/workout.ts
+++ src/features/workouts/types/workout.ts
@@ -1,15 +1,20 @@
-export type WorkoutStatus = 'ACTIVE' | 'INACTIVE' | 'ARCHIVED'
+export type WorkoutStatus = "ACTIVE" | "INACTIVE" | "ARCHIVED";
 
 export type Workout = {
-  workoutId: number
-  teacherId: number
-  workoutName: string
-  status: WorkoutStatus
-  createdAt: string
-  updatedAt: string
-}
+  workoutId: number;
+  teacherId: number;
+  workoutName: string;
+  status: WorkoutStatus;
+  createdAt: string;
+  updatedAt: string;
+};
 
 export type CreateWorkoutRequest = {
-  teacherId: number
-  workoutName: string
-}
+  teacherId: number;
+  workoutName: string;
+};
+
+export type UpdateWorkoutRequest = {
+  workoutName: string;
+  status: WorkoutStatus;
+};
